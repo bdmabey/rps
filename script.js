421308953afab1ca509b6b544360320b91b5bd26
Braden Mabey <braden.mabey@gmail.com>
2021-06-29 12:23:12 -0400

    Added functions to play a around and a game with x rounds.

--- script.js
+++ script.js
@@ -4,3 +4,50 @@ function computerPlay() {
 
     return computerHand;
 }
+
+function playRound(playerSelection, computerSelection) {
+    let loss = 0;
+    let win = 1;
+    let draw = 2;
+
+    switch (true) {
+        case playerSelection.toLowerCase() == "rock" && computerSelection == "scissors":
+        case playerSelection.toLowerCase() == "paper" && computerSelection == "rock":
+        case playerSelection.toLowerCase() == "scissors" && computerSelection == "paper":
+            return win;
+        case playerSelection.toLowerCase() == "rock" && computerSelection == "paper":
+        case playerSelection.toLowerCase() == "paper" && computerSelection == "scissors":
+        case playerSelection.toLowerCase() == "scissors" && computerSelection == "rock":
+            return loss;
+        default:
+            return draw;
+    }
+}
+
+function game() {
+    let timesToLoop = parseInt(prompt("Enter how many rounds you want to play."));
+    let playerPoints = 0;
+    let computerPoints = 0;
+
+    for (let i = 1; i <= timesToLoop; i++) {
+        let playerSelection = prompt("Please enter rock, paper, or scissors.");
+        let computerSelection = computerPlay();
+        switch (playRound(playerSelection, computerSelection)) {
+            case 0:
+                computerPoints += 1;
+                break;
+            case 1:
+                playerPoints += 1;
+                break;
+            case 2:
+                break;
+        }
+    }
+    if (playerPoints > computerPoints) {
+        console.log("You won the game!");
+    } else if (computerPoints > playerPoints) {
+        console.log("You lost the game.");
+    } else {
+        console.log("You two tied.");
+    }
+}
